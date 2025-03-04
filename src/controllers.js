ee16f7ea4dc5496828f797a85efd257fc2e7f1bc
roneljimenez <ronel_jimenez@hotmail.com>
2025-03-03 22:06:18 -0300

    added enpoints for get client bookings from his email

--- src/controllers.js
+++ src/controllers.js
@@ -44,6 +44,34 @@ async function getClients(req, res) {
 
 }
 
+async function getClientId(email, res){
+  try {
+    
+    const response = await axios.get(`${URL_AGENDAPRO}/clients?search=${email}`, {
+      auth: {
+        username: AGENDAPRO_USERNAME,
+        password: AGENDAPRO_PASSWORD
+      }
+    });
+    //¿Es posible que pueda responder más de uno registro con el mismo correo?
+    return response.data[0].id;
+  } catch (error) {
+    console.error("Error en getClientId:", error.message);
+      
+      // Si axios devuelve un error de respuesta (por ejemplo, 401, 404, etc.)
+      if (error.response) {
+        return res.status(error.response.status).json({
+          error: "Error en la API de AgendaPro",
+          details: error.response.data
+        });
+      }
+
+      // Si hay otro tipo de error (por ejemplo, timeout o error de conexión)
+      res.status(500).json({ error: "Error al obtener id del cliente" });
+  }
+
+}
+
 async function getClientBookings(req, res) {
 
   try {
@@ -51,17 +79,23 @@ async function getClientBookings(req, res) {
       if (!URL_AGENDAPRO || !AGENDAPRO_USERNAME || !AGENDAPRO_PASSWORD) {
         return res.status(500).json({ error: "Faltan variables de entorno en el servidor" });
       }
-      const clientId = req.params.id ? req.params.id : "";
+      
+      if(req.query.search && req.query.search!="") {
 
-      // Realizar la petición a AgendaPro con async/await
-      const response = await axios.get(`${URL_AGENDAPRO}/clients/${clientId}/bookings`, {
-        auth: {
-          username: AGENDAPRO_USERNAME,
-          password: AGENDAPRO_PASSWORD
-        }
-      });
-      // Responder con los datos obtenidos
-      return res.json({ data: response.data });
+        const clientId = await getClientId(req.query.search);
+  
+        // Realizar la petición a AgendaPro con async/await
+        const response = await axios.get(`${URL_AGENDAPRO}/clients/${clientId}/bookings`, {
+          auth: {
+            username: AGENDAPRO_USERNAME,
+            password: AGENDAPRO_PASSWORD
+          }
+        });
+        // Responder con los datos obtenidos
+        return res.status(200).json({ bookings: response.data });
+      }else{
+        res.status(500).json({ error: "Error al obtener las reservas del cliente" });
+      }
 
   } catch (error) {
       console.error("Error en getClientBookings:", error.message);
@@ -75,9 +109,9 @@ async function getClientBookings(req, res) {
       }
 
       // Si hay otro tipo de error (por ejemplo, timeout o error de conexión)
-      res.status(500).json({ error: "Error al obtener los clientes" });
+      res.status(500).json({ error: "Error al obtener las reservas del cliente" });
   }
 
 }
 
-module.exports = {getClients, getClientBookings};
+module.exports = {getClients, getClientBookings, getClientId};
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { validateToken } = require("./middleware");
-const { getClients,  getClientBookings } = require("./controllers");
+const { getClients,  getClientBookings, getClientId } = require("./controllers");
 
 const router = express.Router();
 //asi es con validacion de JWT, de momento lo dejaremos abierto para efectos de pruebas de desarrollo
@@ -10,7 +10,10 @@ const router = express.Router();
 //Obtiene lista de clientes
 router.get("/clients", getClients);
 
-router.get("/client/:id/bookings", getClientBookings);
+//obtiene el id de un cliente
+router.get("/getClientId/", getClientId);
 
+//obtiene las reservas de un cliente
+router.get("/client/bookings/", getClientBookings);
 
 module.exports = router;
